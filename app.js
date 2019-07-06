c38453eb9cb7dcff32d4050aff6dfedc6716f258
Siddhant Bohra <abhisidds@gmail.com>
2019-07-07 02:03:34 +0530

    added POST method

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.use(bodyParser.json())
 //Import Routes
 const rulesAdd = require('./routes/rule')
 const workflows = require('./routes/workflow')
+const process = require('./routes/process')
 app.use('/rule',rulesAdd)
 app.use('/workflow',workflows)
+app.use('/process',process)
 //Connect To DB
 mongoose.connect("mongodb+srv://assassin:chromium360@cluster0-fft3g.mongodb.net/test?retryWrites=true&w=majority",
     { useNewUrlParser: true },
--- routes/process.js
+++ routes/process.js
@@ -1,3 +1,52 @@
 const mongoose = require('mongoose')
+const router = express.Router()
+const Procs = require('../models/processSchema')
+require('../app')
 
 
+router.post('/add', (req, res) => {
+
+    if (mongoose.connection.readyState == 1) {
+        console.log(mongoose.connection.readyState)
+        mongoose.connection.db.collection("works", function (err, collection) {
+            collection.find({}).toArray(function (err, data) {
+                console.log(data); // it will print your collection data
+                // let obj = {
+                //     "workflow_id": req.body.workflow_id,
+                //     "workflow_name": req.body.workflow_name,
+                //     "rule_set": req.body.rule_set,
+                //     "opeworkflows_AND_or_OR_conditionrator": req.body.opeworkflows_AND_or_OR_conditionrator
+                // }
+                // let ruleArray = obj.rule_set
+                // for (i = 0; i < ruleArray.length; i++) {
+                //     let result = data.filter((x) => x.rule_name === ruleArray[i].trim());
+                //     newArr.push(result[0])
+                // }
+                // const work = new Work({
+                //     "workflow_id": req.body.workflow_id,
+                //     "workflow_name": req.body.workflow_name,
+                //     "rule_set": newArr,
+                //     "opeworkflows_AND_or_OR_conditionrator": req.body.opeworkflows_AND_or_OR_conditionrator
+
+                // })
+                // work.save().then((result) => {
+                //     res.json({
+                //         success: true,
+                //         result
+                //     });
+                // }).catch((error) => {
+                //     console.log(error)
+                //     res.json({
+                //         error:
+                //         {
+                //             message: "We are facing some issues! Please try again later"
+                //         }
+                //     })
+                // })
+
+            })
+        })
+    }
+})
+
+module.exports = router
